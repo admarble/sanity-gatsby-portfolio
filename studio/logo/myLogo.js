cc380a372ba3ab2618bf624cac960d00ab8caf05
Antonio <admarble@gmail.com>
2019-12-15 16:48:49 -0600

    Added Logo to Studio

---
+++ studio/logo/myLogo.js
@@ -0,0 +1,10 @@
+// ./logo/myLogo.js
+
+import React from 'react';
+import styles from './myLogo.css';
+
+const myLogo = () => (
+  <img className={styles.myLogo} src="/static/am-logo2.svg" alt="Antonio Marble" />
+  );
+
+export default myLogo;
