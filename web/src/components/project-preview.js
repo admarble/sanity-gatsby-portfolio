60f1cb977ed08d448db1895464c49805932f2197
Antonio <admarble@gmail.com>
2019-12-25 13:15:22 -0500

    Updated Project Image Preview Size

--- web/src/components/project-preview.js
+++ web/src/components/project-preview.js
@@ -14,7 +14,7 @@ function ProjectPreview (props) {
         {props.mainImage && props.mainImage.asset && (
           <img
             src={imageUrlFor(buildImageObj(props.mainImage))
-              .width(600)
+              .maxWidth(600)
               .height(Math.floor((9 / 16) * 600))
               .url()}
             alt={props.mainImage.alt}
